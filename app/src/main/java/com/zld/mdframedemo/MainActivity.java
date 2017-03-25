564cb19b9b8b1c7eb38c63341dc274defe6873c6
devzld <779724606@qq.com>
2017-03-25 23:20:15 +0800

    基本实现

--- app/src/main/java/com/zld/mdframedemo/MainActivity.java
+++ app/src/main/java/com/zld/mdframedemo/MainActivity.java
@@ -1,13 +1,69 @@
 package com.zld.mdframedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.jaeger.library.StatusBarUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabs)
+    TabLayout tabs;
+    @BindView(R.id.vp_content)
+    ViewPager vpContent;
+    @BindView(R.id.navigation)
+    NavigationView navigation;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setToolbar();
+        StatusBarUtil.setTransparent(this);
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        //菜单按钮可用
+        actionBar.setHomeButtonEnabled(true);
+        //回退按钮可用
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //将drawerlayout和toolbar绑在一起
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        //初始化状态
+        drawerToggle.syncState();
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+    }
+
+    /**
+     * 给toolbar添加菜单
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
